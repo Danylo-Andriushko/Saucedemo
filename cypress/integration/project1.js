ae1d4ffdee21cb464e8039fb828024435e9720ac
Danylo Andriushko <startupbzns@gmail.com>
2022-05-01 21:28:17 +0300

    test Saucedemo

--- cypress/integration/project1.js
+++ cypress/integration/project1.js
@@ -5,7 +5,7 @@ describe('Test suite', ()=>{
         cy.xpath('//*[@id="password"]').type('secret_sauce')
         cy.xpath('//*[@id="login-button"]').click()
     })
-    it('Test2: Add products into the shopping curt',() => {
+    it('Test2: Add products into the shopping cart',() => {
         cy.wait(1000)
         cy.xpath('//*[@class="btn btn_primary btn_small btn_inventory"]')
         .each((element)=>{
@@ -13,14 +13,14 @@ describe('Test suite', ()=>{
         })
     })
 
-    it('Test3: Delete products from the shopping curt',() => {
+    it('Test3: Delete products from the shopping cart',() => {
         cy.wait(1000)
         cy.xpath('//*[@class="btn btn_secondary btn_small btn_inventory"]')
         .each((element)=>{
             element.click()
         })
     })
-    it('Tes4: Visit shopping curt', () => {
+    it('Tes4: Visit shopping cart', () => {
         cy.wait(1000)
         cy.xpath('//*[@class="shopping_cart_link"]').click()
         if(cy.xpath('//*[@class="error-message-container error"]')){
